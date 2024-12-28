c9d68e3c92fe68992decb592e54f262df2e21dee
Shilpa Mali <malishilpa037@gmail.com>
2024-12-28 14:48:08 +0530

    FindFirst() method and FindAny() method

---
+++ src/com/stream/find/any/StreamAPI.java
@@ -0,0 +1,22 @@
+package com.stream.find.any;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class StreamAPI {
+	public static void main(String[] args) {
+
+		List<Employee> list = new ArrayList<Employee>();
+		list.add(new Employee(101, "sohan", 40000));
+		list.add(new Employee(102, "sandip", 45000));
+		list.add(new Employee(103, "sagar", 55000));
+		list.add(new Employee(104, "samadhan", 50000));
+		list.add(new Employee(105, "shantanu", 35000));
+
+		Optional<Employee> emp = list.stream().findAny();
+		emp.ifPresent(e -> System.out.println(e));
+
+	}
+
+}
